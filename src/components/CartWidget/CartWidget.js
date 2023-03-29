9642a7e007c481c968cc30a34197614a68a9c4ed
EASYva <eduardosagradaai@gmail.com>
2023-03-28 22:10:59 -0300

    PreEntrega1Sagrada

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,14 @@
+import carrito from './assets/carrito.svg'
+const CartWidget = () =>{
+return(
+    <div className='cart-widget'>
+        <img src={carrito} alt="cart-widget"/>
+          0  
+    
+    </div>
+)
+}
+
+export default CartWidget
+
+
---
+++ src/components/HomeIcon/HomeIcon.js
@@ -0,0 +1,9 @@
+import homeIcon from "./mainicon.svg";
+    const HomeIcon = () =>{
+        return(
+            <div className="home-icon">
+                <img src={homeIcon} alt="home-icon"/>
+            </div>
+        )
+    }
+    export default HomeIcon;
